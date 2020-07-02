475441ac0dd51d80ca8ee47bc5ebe2ce7087e66f
wjb939790671 <939790671@qq.com>
2020-07-03 07:30:18 +0800

    2

--- controllers/default.go
+++ controllers/default.go
@@ -30,3 +30,44 @@ func (this *MainController) Get() {
 	this.Data["Meun"] = list
 	this.TplName = "index.html"
 }
+func (this *MainController) LoadMeun() {
+	meunId, error := this.GetInt("meunId")
+	if error != nil {
+		return
+	}
+	tree := getTree(this.model, this.iBll, meunId)
+	this.Data["json"] = tree
+	this.ServeJSON()
+
+}
+
+type Tree struct {
+	Id         int                    `json:"id"`
+	Text       string                 `json:"text"`
+	IconCls    string                 `json:"iconCls"`
+	State      string                 `json:"State"`
+	Checked    bool                   `json:"checked"`
+	Attributes map[string]interface{} `json:"attributes"`
+	Children   []Tree                 `json:"children"`
+}
+
+func getTree(model interface{}, iBll bll.IBll, parentId int) []Tree {
+	where := make(map[string]interface{})
+	where["ParentId"] = parentId
+	var list []models.Action
+	iBll.QueryAll(model, &list, where, []string{"Id", "ActionName"}, "Id", "ActionName", "Icon", "Url", "IsMeun", "M")
+	var trees []Tree
+	for _, value := range list {
+		var tree Tree
+		if value.IsMeun {
+			tree.Children = getTree(model, iBll, value.Id)
+		}
+		atrr := make(map[string]interface{})
+		atrr["url"] = value.Url
+		tree.Id = value.Id
+		tree.Text = value.ActionName
+		tree.IconCls = value.Icon
+		trees = append(trees, tree)
+	}
+	return trees
+}
--- routers/router.go
+++ routers/router.go
@@ -2,9 +2,11 @@ package routers
 
 import (
 	"OA/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/loadmeun", &controllers.MainController{}, "post:LoadMeun")
 }
