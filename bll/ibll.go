611fc9109ba45329e2692bead91ce2abd54f03f3
wjb939790671 <939790671@qq.com>
2020-07-03 04:55:46 +0800

    2

--- bll/ibll.go
+++ bll/ibll.go
@@ -10,6 +10,6 @@ type IBll interface {
 	QueryTable(model, list interface{}, where map[string]interface{}, relatedSel []string, orderBy []string, pageSize, pageIndex int, cols ...string) int64
 	QueryAll(model, list interface{}, where map[string]interface{}, orderBy []string, cols ...string)
 	//M2MAdd(model, dataModel interface{}, relatedSel string) (int64, error)
-	M2MQuery(model interface{}, relatedSel string) (int64, error)
+	//M2MQuery(model interface{}, relatedSel string) (int64, error)
 	// IsExist(model interface{}, cols ...string) bool
 }
